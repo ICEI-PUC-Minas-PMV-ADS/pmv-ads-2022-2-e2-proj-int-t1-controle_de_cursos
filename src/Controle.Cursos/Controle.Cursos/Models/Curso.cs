44e43a8b402312e84319353bd0d3093dadf33164
Vitor Martins <vitormartins.one@gmail.com>
2022-10-08 14:20:35 -0300

    criado model curso.cs e classe de constantes para strings de erro

---
+++ src/Controle.Cursos/Controle.Cursos/Models/Curso.cs
@@ -0,0 +1,17 @@
+﻿using Controle.Cursos.Constantes;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Controle.Cursos.Models
+{
+    [Table("Curso")]
+    public class Curso
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage = ErrorMessage.NOME_OBRIGATORIO)]
+        public string Nome { get; set; }
+        public DateTime Horario { get; set; }
+    }
+}
