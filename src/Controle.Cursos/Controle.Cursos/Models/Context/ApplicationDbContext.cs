f97f223656d33cf9bcc423155b3a06051cd0c5f3
Vitor Martins <vitormartins.one@gmail.com>
2022-10-08 14:33:14 -0300

    bibliotecas entity framework core e dbcontext

---
+++ src/Controle.Cursos/Controle.Cursos/Models/Context/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Controle.Cursos.Models.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(
+            DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
