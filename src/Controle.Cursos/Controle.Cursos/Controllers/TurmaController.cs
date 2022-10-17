3e796c4bbd9bedeea6e88d68104723fa8004c282
Vitor Martins <vitormartins.one@gmail.com>
2022-10-16 21:04:36 -0300

    turma e solicitacao controller e partial

---
+++ src/Controle.Cursos/Controle.Cursos/Controllers/TurmaController.cs
@@ -0,0 +1,184 @@
+﻿using Controle.Cursos.Models;
+using Controle.Cursos.Models.Context;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Controle.Cursos.Controllers
+{
+    public class TurmaController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public TurmaController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Turmas.ToListAsync());
+        }
+        
+        public async Task<IActionResult> DetailsAsync(int id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var turma = await _context.Turmas
+                .FirstOrDefaultAsync(c => c.Id == id);
+
+            if (turma == null)
+            {
+                return NotFound();
+            }
+
+            return View(turma);
+        }
+
+        public ActionResult Create()
+        {
+            var cursos = _context.Cursos.ToList();
+
+            if (cursos != null)
+            {
+                ViewBag.Cursos = cursos;
+            }
+
+            var solicitacoes = _context.Solicitacoes.ToList();
+
+            if (solicitacoes != null)
+            {
+                ViewBag.Solicitacoes = solicitacoes;
+            }
+
+            return View();
+        }
+
+        public ActionResult SolicitacaoPartial(
+            [Bind("Id")] int cursoId)
+        {
+            var cursos = _context.Cursos.ToList();
+
+            if (cursos != null)
+            {
+                ViewBag.Cursos = cursos;
+            }
+
+            var solicitacoes = _context.Solicitacoes
+                .Select(s => s)
+                .Where(s => s.CursoId == cursoId)
+                .AsEnumerable();
+
+            if (solicitacoes != null)
+            {
+                ViewBag.Solicitacoes = solicitacoes;
+            }
+
+            return View("Create");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(
+            [Bind("Id, AlunoId, CursoId")] Turma turma)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(turma);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(turma);
+        }
+
+        public async Task<IActionResult> EditAsync(int id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var turma = await _context.Turmas.FindAsync(id);
+
+            if (turma == null)
+            {
+                return NotFound();
+            }
+
+            return View(turma);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> EditAsync(int id, 
+            [Bind("Id, AlunoId, CursoId")] Turma turma)
+        {
+            if (id != turma.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(turma);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TurmaExiste(turma.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(turma);
+        }
+
+        public async Task<IActionResult> DeleteAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var turma = await _context.Turmas
+                .FirstOrDefaultAsync(c => c.Id == id);
+
+            if (turma == null)
+            {
+                return NotFound();
+            }
+
+            return View(turma);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteAsync(int id, IFormCollection collection)
+        {
+            var turma = await _context.Turmas.FindAsync(id);
+            _context.Turmas.Remove(turma);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool TurmaExiste(int id)
+        {
+            return _context.Turmas.Any(e => e.Id == id);
+        }
+    }
+}
--- src/Controle.Cursos/Controle.Cursos/Models/Turma.cs
+++ src/Controle.Cursos/Controle.Cursos/Models/Turma.cs
@@ -6,7 +6,6 @@ namespace Controle.Cursos.Models
     public class Turma
     {
         public int Id { get; set; }
-        public string Nome { get; set; }
         public int CursoId { get; set; }
         public virtual Curso Curso { get; set; }
         public virtual IEnumerable<Aluno> Alunos { get; set; }
