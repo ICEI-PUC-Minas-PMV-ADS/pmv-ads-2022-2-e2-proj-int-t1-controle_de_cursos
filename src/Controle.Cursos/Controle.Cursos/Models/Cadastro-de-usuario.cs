b64c92f1b1345c4b6dd090b52138354484cb843b
Pedro Guimarães Santos <80483432+pecosaints@users.noreply.github.com>
2022-10-30 19:07:22 -0300

    Add files via upload

---
+++ src/Controle.Cursos/Controle.Cursos/Models/Cadastro-de-usuario.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Controle.Cursos.Models
+{
+    [Table("Cadastro_Usuario")]
+    public class Cadastro_de_usuario
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar o nome!")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar o email!")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar a senha!")]
+        public string Senha { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar o perfil!")]
+        public Perfil Perfil { get; set; }
+    }
+
+    public enum Perfil
+    {
+        Admin,
+        User
+    }
+
+}
