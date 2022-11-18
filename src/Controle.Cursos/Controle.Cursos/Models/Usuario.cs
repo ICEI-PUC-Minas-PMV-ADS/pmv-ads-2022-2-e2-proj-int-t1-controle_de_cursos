ec530d9f3037a3b35d542c0e86b504ab057337c0
EGC <renato.ferreira.66002@sga.pucminas.br>
2022-11-18 20:04:57 -0300

    feat: Adiciona o model Usuario.

---
+++ src/Controle.Cursos/Controle.Cursos/Models/Usuario.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Controle.Cursos.Models
+{
+    [Table("Usuários")]
+    public class Usuario
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar o nome!")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar a senha!")]
+        [DataType(DataType.Password)]
+        public string Senha { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar o perfil!")]
+        public Perfil Perfil { get; set; }
+    }
+
+    public enum Perfil
+    {
+        Admin,
+        User
+    }
+}
