69e14f759feedd9a406e559297742927d769988f
EGC <renato.ferreira.66002@sga.pucminas.br>
2022-11-18 20:27:47 -0300

    feat: Adicionar ApplicationDbContext.

---
+++ src/Controle.Cursos/Controle.Cursos/Models/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Controle.Cursos.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Aluno> Alunos { get; set; }
+        public DbSet<Curso> Cursos { get; set; }
+        public DbSet<Fornecedor> Fornecedores { get; set; }
+        public DbSet<Solicitacao> Solicitacoes { get; set; }
+        public DbSet<Turma> Turmas { get; set; }
+        public DbSet<Usuario> Usuarios { get; set; }
+    }
+
+}
