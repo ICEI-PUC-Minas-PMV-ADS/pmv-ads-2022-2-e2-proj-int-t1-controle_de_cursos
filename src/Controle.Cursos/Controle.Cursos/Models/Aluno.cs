b25a9c065903fb7a53e5506b65e11164139199bc
Rafael Penido <rafaelpenido1998@gmail.com>
2022-11-28 10:13:07 -0300

    corrigindo display name fornecedor e aluno

--- src/Controle.Cursos/Controle.Cursos/Models/Aluno.cs
+++ src/Controle.Cursos/Controle.Cursos/Models/Aluno.cs
@@ -1,6 +1,7 @@
 ﻿using Controle.Cursos.Models.Enum;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 
 namespace Controle.Cursos.Models
 {
@@ -9,6 +10,7 @@ namespace Controle.Cursos.Models
         public int Id { get; set; }
         public string Nome { get; set; }
         public string Cpf { get; set; }
+        [DisplayName("Data de Nascimento")]
         public DateTime DataNascimento { get; set; }
         public virtual IEnumerable<Turma> Turmas { get; set; }
         public virtual IEnumerable<Solicitacao> Solicitacoes { get; set; }
--- src/Controle.Cursos/Controle.Cursos/Models/Fornecedor.cs
+++ src/Controle.Cursos/Controle.Cursos/Models/Fornecedor.cs
@@ -1,13 +1,18 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel;
 
 namespace Controle.Cursos.Models
 {
     public class Fornecedor
     {
         public int Id { get; set; }
+        [DisplayName("Cnpj")]
         public string Cnpj { get; set; }
+        [DisplayName("Razão Social")]
         public string RazaoSocial { get; set; }
+        [DisplayName("Nome Fantasia")]
         public string NomeFantasia { get; set; }
+        [DisplayName("Inscrição Estadual")]
         public int? InscricaoEstadual { get; set; }
         public virtual List<Curso> Cursos { get; set; }
     }
